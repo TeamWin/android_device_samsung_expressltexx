f0cb7dc7c7552403470a8bdbaed40fdbc7a40d2a
szezso <szezsoo@gmail.com>
2014-10-23 15:15:06 +0200

    Expressltexx: Typo

--- device.mk
+++ device.mk
@@ -36,7 +36,7 @@ DEVICE_PACKAGE_OVERLAYS += device/samsung/expressltexx/overlay
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
 
 # Audio configuration
